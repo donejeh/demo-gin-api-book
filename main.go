c0526b999ae8ee102295d8be5c441e37d27ae9ed
DON EJEH1~ <donejeh@gmail.com>
2024-01-04 13:14:12 +0100

    bookById

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ type book struct {
 	Quantity int    `json:"quantity"`
 }
 
-
 var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
@@ -25,7 +24,6 @@ var books = []book{
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
-
 
 func bookById(c *gin.Context) {
 	id := c.Param("id")
@@ -37,4 +35,78 @@ func bookById(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusOK, book)
+}
+
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
+		return
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func returnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+
+	return nil, errors.New("book not found")
+}
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
+	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnBook)
+	router.Run("localhost:8080")
 }
